b28772f4afc639c0b40bf41c4b66c6afbe7caaa5
Allan <allanfelipedk@gmail.com>
2024-05-26 10:04:09 -0300

    feat: realizando os calculos

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,11 +1,17 @@
 function adicionar() {
-    let nomeProduto = document.getElementById("produto");
-    let quantidade = document.getElementById("quantidade");
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade');
 
-    alert(nomeProduto.value);
+    alert(nomeProduto);
+    alert(valorUnitario);
     alert(quantidade.value);
 
+    let preco = quantidade.value * valorUnitario;
+    alert(preco);
 }
+//Código omitido
 
 function limpar() {
 
