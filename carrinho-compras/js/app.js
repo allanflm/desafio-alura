97a470d09e61ae9a39b5c7875271b3a8631f99d6
Allan <allanfelipedk@gmail.com>
2024-05-26 09:45:35 -0300

    feat: função para adicionar o produto

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,12 @@
+function adicionar() {
+    let nomeProduto = document.getElementById("produto");
+    let quantidade = document.getElementById("quantidade");
+
+    alert(nomeProduto.value);
+    alert(quantidade.value);
+
+}
+
+function limpar() {
+
+}
