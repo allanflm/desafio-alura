ebf08d2da79010f355943eb62555b08278080d23
Allan <allanfelipedk@gmail.com>
2024-05-26 10:12:11 -0300

    feat: adicionando os produto no carrinho

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -2,14 +2,16 @@ function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
+    let quantidade = document.getElementById('quantidade').value;
 
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
+    let preco = quantidade * valorUnitario;
 
-    let preco = quantidade.value * valorUnitario;
-    alert(preco);
+    let carrinho = document.getElementById("lista-produtos");
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`;
+
+
 }
 //Código omitido
 
